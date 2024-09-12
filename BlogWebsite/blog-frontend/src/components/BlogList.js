6beda2cf6b272338f98e096f3f153e8c455877ff
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-09-12 14:18:18 +0530

    Commit

--- BlogWebsite/blog-frontend/src/components/BlogList.js
+++ BlogWebsite/blog-frontend/src/components/BlogList.js
@@ -1,34 +1,34 @@
-// import React, { useEffect, useState } from 'react';
-// import axios from 'axios';
-// import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 
-// const BlogList = () => {
-//   const [posts, setPosts] = useState([]);
+const BlogList = () => {
+  const [posts, setPosts] = useState([]);
 
-//   useEffect(() => {
-//     axios.get('/api/blog')
-//       .then(response => setPosts(response.data))
-//       .catch(error => console.error(error));
-//   }, []);
+  useEffect(() => {
+    axios.get('/api/blog')
+      .then(response => setPosts(response.data))
+      .catch(error => console.error(error));
+  }, []);
 
-//   return (
-//     <div>
-//       <h2>All Blog Posts</h2>
+  return (
+    <div>
+      <h2>All Blog Posts</h2>
       
-//       {/* Add Create Post Button */}
-//       <Link to="/create">
-//         <button>Create New Post</button>
-//       </Link>
+      {/* Add Create Post Button */}
+      <Link to="/create">
+        <button>Create New Post</button>
+      </Link>
 
-//       {posts.map(post => (
-//         <div key={post._id}>
-//           <h3>{post.title}</h3>
-//           <p>{post.content.substring(0, 100)}...</p>
-//           <Link to={`/post/${post._id}`}>Read More</Link>
-//         </div>
-//       ))}
-//     </div>
-//   );
-// };
+      {posts.map(post => (
+        <div key={post._id}>
+          <h3>{post.title}</h3>
+          <p>{post.content.substring(0, 100)}...</p>
+          <Link to={`/post/${post._id}`}>Read More</Link>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-// export default BlogList;
+export default BlogList;
