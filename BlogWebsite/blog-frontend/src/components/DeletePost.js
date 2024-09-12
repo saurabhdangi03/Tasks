392f6e776e74c8e803d98c685032767e36e6d36c
saurabhdangi03 <104308074+saurabhdangi03@users.noreply.github.com>
2024-09-12 21:51:35 +0530

    Commit

--- BlogWebsite/blog-frontend/src/components/DeletePost.js
+++ BlogWebsite/blog-frontend/src/components/DeletePost.js
@@ -1,17 +1,17 @@
-// import axios from 'axios';
+import axios from 'axios';
 
-// const DeletePost = ({ id }) => {
-//   const handleDelete = () => {
-//     axios.delete(`/api/blog/posts/${id}`)
-//       .then(() => {
-//         window.location.reload(); // Reload the page or redirect after delete
-//       })
-//       .catch(error => {
-//         console.error('There was an error deleting the post!', error);
-//       });
-//   };
+const DeletePost = ({ id }) => {
+  const handleDelete = () => {
+    axios.delete(`/api/blog/posts/${id}`)
+      .then(() => {
+        window.location.reload(); // Reload the page or redirect after delete
+      })
+      .catch(error => {
+        console.error('There was an error deleting the post!', error);
+      });
+  };
 
-//   return <button onClick={handleDelete}>Delete Post</button>;
-// };
+  return <button onClick={handleDelete}>Delete Post</button>;
+};
 
-// export default DeletePost;
+export default DeletePost;
